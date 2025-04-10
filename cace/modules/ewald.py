a1525bc90405d585f3b36f60e7d0c3c72b8b0683
djkim-kr <dongjin.kim@berkeley.edu>
2025-04-09 17:38:46 -0700

    ewald optimzation in batchsize > 1

--- cace/modules/ewald.py
+++ cace/modules/ewald.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from itertools import product
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 import numpy as np
 
 class EwaldPotential(nn.Module):
@@ -56,12 +56,6 @@ class EwaldPotential(nn.Module):
         else:
             batch_now = data["batch"]
 
-        # this is just for compatibility with the previous version
-        if hasattr(self, 'exponent') == False:
-            self.exponent = 1
-        if hasattr(self, 'compute_field') == False:
-            self.compute_field = False
-        
         # box = data['cell'].view(-1, 3, 3).diagonal(dim1=-2, dim2=-1)
         box = data['cell'].view(-1, 3, 3)
         r = data['positions']
@@ -76,56 +70,82 @@ class EwaldPotential(nn.Module):
 
         unique_batches = torch.unique(batch_now)  # Get unique batch indices
 
-        results = []
-        field_results = []
-        for i in unique_batches:
-            mask = batch_now == i  # Create a mask for the i-th configuration
-            # Calculate the potential energy for the i-th configuration
-            r_raw_now, q_now, box_now = r[mask], q[mask], box[i]
-            box_diag = box[i].diagonal(dim1=-2, dim2=-1)
-            if box_diag[0] < 1e-6 and box_diag[1] < 1e-6 and box_diag[2] < 1e-6 and self.exponent == 1:
-                # the box is not periodic, we use the direct sum
-                pot, field = self.compute_potential_realspace(r_raw_now, q_now, self.compute_field)
-            elif box_diag[0] > 0 and box_diag[1] > 0 and box_diag[2] > 0:
-                # the box is periodic, we use the reciprocal sum
-                pot, field = self.compute_potential_triclinic(r_raw_now, q_now, box_now, self.compute_field)
-            else:
-                raise ValueError("Either all box dimensions must be positive or aperiodic box must be provided.")
+        # this is just for compatibility with the previous version
+        if hasattr(self, 'exponent') == False:
+            self.exponent = 1
+        if hasattr(self, 'compute_field') == False:
+            self.compute_field = False
+        if unique_batches.shape[0] > 1:
+            # if we have multiple batches, we need to compute in parallel, faster.
+            #torch.jit.fork/wait -> asyncchrounously GPU kernel(parallel)
+            futures = [torch.jit.fork(self._compute_for_configuration,
+                                        r[batch_now == b],
+                                        q[batch_now == b],
+                                        box[b],
+                                        data,
+                                        int(b))
+                    for b in unique_batches]
+            batch_results = [torch.jit.wait(f) for f in futures]
+        else:
+            batch_results = [self._compute_for_configuration(
+                                        r[batch_now == b],
+                                        q[batch_now == b],
+                                        box[b],
+                                        data,
+                                        int(b))
+                    for b in unique_batches]
 
-            if self.exponent == 1 and hasattr(self, 'external_field') and self.external_field is not None:
-                # if self.external_field_direction is an integer, then external_field_direction is the direction index
-                if isinstance(self.external_field_direction, int):
-                    direction_index_now = self.external_field_direction
-                    # if self.external_field_direction is a string, then it is the key to the external field
-                elif self.external_field_direction in data and data[self.external_field_direction] is not None:
-                    direction_index_now = int(data[self.external_field_direction][i])
-                else:
-                    raise ValueError("external_field_direction must be an integer or a key to the external field")
-                if isinstance(self.external_field, float):
-                    external_field_now = self.external_field
-                elif self.external_field in data and data[self.external_field] is not None:
-                    external_field_now = data[self.external_field][i]
-                else:
-                    raise ValueError("external_field must be a float or a key to the external field")
-                box_now = box_now.diagonal(dim1=-2, dim2=-1)
-                pot_ext = self.add_external_field(r_raw_now, q_now, box_now, direction_index_now, external_field_now)
-            else:
-                pot_ext = 0.0
+        pot_list = [result[0] for result in batch_results]
+        field_list = [result[1] for result in batch_results]
 
-            if hasattr(self, 'charge_neutral_lambda') and self.charge_neutral_lambda is not None:
-                q_mean = torch.mean(q[mask])
-                pot_neutral = self.charge_neutral_lambda * (q_mean)**2.
-                #print(pot_neutral, pot)
-            else:
-                pot_neutral = 0.0
-
-            results.append(pot + pot_ext + pot_neutral)
-            field_results.append(field)
-
-        data[self.output_key] = torch.stack(results, dim=0).sum(dim=1) if self.aggregation_mode == "sum" else torch.stack(results, dim=0)
+        data[self.output_key] = torch.stack(pot_list, dim=0).sum(dim=1)
         if self.compute_field:
-            data[self.feature_key+'_field'] = torch.cat(field_results, dim=0)
+            data[self.feature_key+'_field'] = torch.cat(field_list, dim=0)
         return data
+
+    def _compute_for_configuration(self,
+                               r_config: torch.Tensor,
+                               q_config: torch.Tensor,
+                               box_config: torch.Tensor,
+                               data: Dict[str, torch.Tensor],
+                               batch_index: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        box_diag = box_config.diagonal(dim1=-2, dim2=-1)
+        if box_diag[0] < 1e-6 and box_diag[1] < 1e-6 and box_diag[2] < 1e-6 and self.exponent == 1:
+            # the box is not periodic, we use the direct sum
+            pot, field = self.compute_potential_realspace(r_config, q_config, self.compute_field)
+        elif box_diag[0] > 0 and box_diag[1] > 0 and box_diag[2] > 0:
+            # the box is periodic, we use the reciprocal sum
+            pot, field = self.compute_potential_triclinic(r_config, q_config, box_config, self.compute_field)
+        else:
+            raise ValueError("Either all box dimensions must be positive or aperiodic box must be provided.")
+        
+        if self.exponent == 1 and hasattr(self, 'external_field') and self.external_field is not None:
+            # if self.external_field_direction is an integer, then external_field_direction is the direction index
+            if isinstance(self.external_field_direction, int):
+                direction_index_now = self.external_field_direction
+                # if self.external_field_direction is a string, then it is the key to the external field
+            elif self.external_field_direction in data and data[self.external_field_direction] is not None:
+                direction_index_now = int(data[self.external_field_direction][batch_index])
+            else:
+                raise ValueError("external_field_direction must be an integer or a key to the external field")
+            if isinstance(self.external_field, float):
+                external_field_now = self.external_field
+            elif self.external_field in data and data[self.external_field] is not None:
+                external_field_now = data[self.external_field][batch_index]
+            else:
+                raise ValueError("external_field must be a float or a key to the external field")
+            box_now = box_config.diagonal(dim1=-2, dim2=-1)
+            pot_ext = self.add_external_field(r_config, q_config, box_now, direction_index_now, external_field_now)
+        else:
+            pot_ext = 0.0
+
+        if hasattr(self, 'charge_neutral_lambda') and self.charge_neutral_lambda is not None:
+            q_mean = torch.mean(q_config)
+            pot_neutral = self.charge_neutral_lambda * (q_mean)**2.
+        else:
+            pot_neutral = 0.0
+            
+        return pot + pot_ext + pot_neutral, field
 
     def compute_potential_realspace(self, r_raw, q, compute_field:bool =False):
         # Compute pairwise distances (norm of vector differences)
@@ -169,179 +189,6 @@ class EwaldPotential(nn.Module):
     
         return pot * self.norm_factor, q_field * self.norm_factor
  
-
-    def compute_potential(self, r_raw, q, box, compute_field=False):
-        """ Compute the Ewald long-range potential for one configuration """
-        dtype = torch.complex64 if r_raw.dtype == torch.float32 else torch.complex128
-        device = r_raw.device
-
-
-        volume = box[0] * box[1] * box[2]
-        r = r_raw / box  # Work with scaled positions
-        # r =  r - torch.round(r) # periodic boundary condition
-
-        # Calculate nk based on the provided box dimensions and resolution
-        nk = (box / self.dl).int().tolist()
-        for i in range(3):
-            if nk[i] < 1: nk[i] = 1
-        n = r.shape[0]
-        eikx = torch.zeros((n, nk[0] + 1), dtype=dtype, device=device)
-        eiky = torch.zeros((n, 2 * nk[1] + 1), dtype=dtype, device=device)
-        eikz = torch.zeros((n, 2 * nk[2] + 1), dtype=dtype, device=device)
-
-        eikx[:, 0] = torch.ones(n, dtype=dtype, device=device)
-        eiky[:, nk[1]] = torch.ones(n, dtype=dtype, device=device)
-        eikz[:, nk[2]] = torch.ones(n, dtype=dtype, device=device)
-
-        # Calculate remaining positive kx, ky, and kz terms by recursion
-        for k in range(1, nk[0] + 1):
-            eikx[:, k] = torch.exp(1j * self.twopi * k * r[:, 0]) 
-        for k in range(1, nk[1] + 1):
-            eiky[:, nk[1] + k] = torch.exp(1j * self.twopi * k * r[:, 1])
-        for k in range(1, nk[2] + 1):
-            eikz[:, nk[2] + k] = torch.exp(1j * self.twopi * k * r[:, 2])
-
-        # Negative k values are complex conjugates of positive ones
-        for k in range(nk[1]):
-            eiky[:, k] = torch.conj(eiky[:, 2 * nk[1] - k])
-        for k in range(nk[2]):
-            eikz[:, k] = torch.conj(eikz[:, 2 * nk[2] - k])
-
-        pot_list = []
-        q_field = torch.zeros_like(q, dtype=r_raw.dtype, device=device) # Field due to q
-        
-        for kx in range(nk[0] + 1):
-            # for negative kx, the Fourier transform is just the complex conjugate of the positive kx
-            factor = 1.0 if kx == 0 else 2.0
-
-            for ky, kz in product(range(-nk[1], nk[1] + 1), range(-nk[2], nk[2] + 1)):
-                k_sq = self.twopi_sq * ((kx / box[0]) ** 2 + (ky / box[1]) ** 2 + (kz / box[2]) ** 2)
-                if k_sq <= self.k_sq_max and k_sq > 0:  # remove the k=0 term
-                    if self.exponent == 1:
-                        kfac = torch.exp(-self.sigma_sq_half * k_sq) / k_sq
-                    elif self.exponent == 6:
-                        b_sq = k_sq * self.sigma_sq_half
-                        b = torch.sqrt(b_sq)
-                        kfac = -1.0 * k_sq**(3/2) * (torch.pi**0.5 * torch.special.erfc(b) + (1 / (2 * b**3) - 1 / b) * torch.exp(-b_sq))
-                    eik = (eikx[:, kx] * eiky[:, nk[1] + ky] * eikz[:, nk[2] + kz]).unsqueeze(1) # [n, 1]
-                    sk = torch.sum(q * eik, dim=0) # [n_q]
-                    sk_conj = torch.conj(sk)
-                    sk_field = 2. * kfac * sk_conj # the factor of 2 comes from normalization factor 2\epsilon
-                    pot_list.append(factor * kfac * torch.real(sk * sk_conj))
-                    if compute_field:
-                        # The reverse transform to get the real-space potential field
-                        q_field += factor * torch.real(sk_field.unsqueeze(0) * eik)
-
-        pot = torch.stack(pot_list).sum(axis=0) / volume
-        if compute_field:
-            q_field /= volume
-        #print(pot, torch.sum(q * q_field, dim=0) /2) #should be the same
-
-        if self.remove_self_interaction and self.exponent == 1:
-            pot -= torch.sum(q ** 2) / (self.sigma * self.twopi**(3./2.))
-            q_field = q_field - q / (self.sigma * self.twopi**(3./2.)) * 2.
-
-        return pot.real * self.norm_factor, q_field * self.norm_factor
-
-    # Optimized function
-    def compute_potential_optimized(self, r_raw, q, box, compute_field=False):
-        dtype = torch.complex64 if r_raw.dtype == torch.float32 else torch.complex128
-        device = r_raw.device
-
-        volume = box[0] * box[1] * box[2]
-        r = r_raw / box
-
-        q_field = torch.zeros_like(q, dtype=r_raw.dtype, device=device) # Field due to q
-
-        nk = (box / self.dl).int().tolist()
-        nk = [max(1, k) for k in nk]
-
-        n = r.shape[0]
-        eikx = torch.ones((n, nk[0] + 1), dtype=dtype, device=device)
-        eiky = torch.ones((n, 2 * nk[1] + 1), dtype=dtype, device=device)
-        eikz = torch.ones((n, 2 * nk[2] + 1), dtype=dtype, device=device)
-
-        eikx[:, 1] = torch.exp(1j * self.twopi * r[:, 0])
-        eiky[:, nk[1] + 1] = torch.exp(1j * self.twopi * r[:, 1])
-        eikz[:, nk[2] + 1] = torch.exp(1j * self.twopi * r[:, 2])
-        # Calculate remaining positive kx, ky, and kz terms by recursion
-        for k in range(2, nk[0] + 1):
-            eikx[:, k] = eikx[:, k - 1].clone() * eikx[:, 1].clone()
-        for k in range(2, nk[1] + 1):
-            eiky[:, nk[1] + k] = eiky[:, nk[1] + k - 1].clone() * eiky[:, nk[1] + 1].clone()
-        for k in range(2, nk[2] + 1):
-            eikz[:, nk[2] + k] = eikz[:, nk[2] + k - 1].clone() * eikz[:, nk[2] + 1].clone()
-
-        # Negative k values are complex conjugates of positive ones
-        for k in range(nk[1]):
-            eiky[:, k] = torch.conj(eiky[:, 2 * nk[1] - k])
-        for k in range(nk[2]):
-            eikz[:, k] = torch.conj(eikz[:, 2 * nk[2] - k])
-
-        kx = torch.arange(nk[0] + 1, device=device)
-        ky = torch.arange(-nk[1], nk[1] + 1, device=device)
-        kz = torch.arange(-nk[2], nk[2] + 1, device=device)
-
-        kx_term = (kx / box[0]) ** 2
-        ky_term = (ky / box[1]) ** 2
-        kz_term = (kz / box[2]) ** 2
-
-        kx_sq = kx_term.view(-1, 1, 1)
-        ky_sq = ky_term.view(1, -1, 1)
-        kz_sq = kz_term.view(1, 1, -1)
-
-        k_sq = self.twopi_sq * (kx_sq + ky_sq + kz_sq) # [nx, ny, nz]
-
-        if self.exponent == 1:
-            kfac = torch.exp(-self.sigma_sq_half * k_sq) / k_sq # [nx, ny, nz]
-        elif self.exponent == 6:
-            # Calculate b_sq and b
-            b_sq = k_sq * self.sigma_sq_half
-            b = torch.sqrt(b_sq)
-
-            # Compute kfac based on the provided expression
-            kfac = -1.0 * k_sq ** (3 / 2) * ( torch.pi ** 0.5 * torch.special.erfc(b) + (1 / (2 * b ** 3) - 1 / b) * torch.exp(-b_sq))
-            #kfac = -1.0 * k_sq ** (3 / 2) * torch.exp(-b_sq) # this assumed a Gaussian smearing
-
-        mask = (k_sq <= self.k_sq_max) & (k_sq > 0)
-        kfac[~mask] = 0
-
-        eikx_expanded = eikx.unsqueeze(2).unsqueeze(3) #[n_node, n_x, 1, 1]
-        eiky_expanded = eiky.unsqueeze(1).unsqueeze(3) #[n_node, 1, n_y, 1]
-        eikz_expanded = eikz.unsqueeze(1).unsqueeze(2) #[n_node, 1, 1, n_z]
-
-        factor = torch.ones_like(kx, dtype=r_raw.dtype, device=device)
-        factor[1:] = 2.0
-
-        if q.dim() == 1:
-            # [n_node, n_q, 1, 1, 1]
-            q_expanded = q.unsqueeze(1).unsqueeze(2).unsqueeze(3).unsqueeze(4)
-        elif q.dim() == 2:
-            q_expanded = q.unsqueeze(2).unsqueeze(3).unsqueeze(4)
-        else:
-            raise ValueError("q must be 1D or 2D tensor")
-        # q_expanded: [n_node, n_q, 1, 1, 1]
-        # eik: [n_node, n_x, n_y, n_z]
-        # sk: [n_q, n_x, n_y, n_z]
-        # kfac: [n_x, n_y, n_z]
-        eik = eikx_expanded * eiky_expanded * eikz_expanded
-        sk = torch.sum(q_expanded * eik.unsqueeze(1), dim=[0])
-        sk_conj = torch.conj(sk)
-        pot = (kfac.unsqueeze(0) * factor.view(1, -1, 1, 1) * torch.real(sk_conj * sk)).sum(dim=[1, 2, 3])
-        # The reverse transform to get the real-space potential field
-        if compute_field:
-            sk_field = 2. * kfac.unsqueeze(0) * sk_conj  # the factor of 2 comes from normalization factor 2\epsilon
-            q_field = (factor.view(1, 1, -1, 1, 1) * torch.real(eik.unsqueeze(1) * sk_field.unsqueeze(0))).sum(dim=[2, 3, 4])
-            q_field /= volume
-
-        pot /= volume
-
-        if self.remove_self_interaction and self.exponent == 1:
-            pot -= torch.sum(q ** 2) / (self.sigma * self.twopi**(3./2.))
-            q_field = q_field - q / (self.sigma * self.twopi**(3./2.)) * 2.
-
-        return pot.real * self.norm_factor, q_field * self.norm_factor
-
     def add_external_field(self, r_raw, q, box, direction_index, external_field):
         external_field_norm_factor = (self.norm_factor/90.0474)**0.5
         # wrap in box
