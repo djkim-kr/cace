64b62610a8fab7a6d4c2e7a824eb787c13465320
djkim-kr <dongjin.kim@berkeley.edu>
2025-08-11 16:21:23 -0700

    multi-head, single-linear layer added

--- cace/modules/atomwise.py
+++ cace/modules/atomwise.py
@@ -1,4 +1,5 @@
 from typing import Dict, Union, Sequence, Callable, Optional
+from unicodedata import name
 
 import torch
 import torch.nn as nn
@@ -6,7 +7,7 @@ import torch.nn.functional as F
 from .blocks import Dense, ResidualBlock, build_mlp
 from ..tools import scatter_sum
 
-__all__ = ["Atomwise"]
+__all__ = ["Atomwise", "Atomwise_linear"]
 
 class Atomwise(nn.Module):
     """
@@ -31,7 +32,10 @@ class Atomwise(nn.Module):
         residual: bool = False,
         use_batchnorm: bool = False,
         add_linear_nn: bool = False,
-        post_process: Optional[Callable] = None
+        post_process: Optional[Callable] = None,
+        per_atom_output_key_2: Optional[str] = None,  # for multi-head output
+        energy_output_index: Optional[int] = None,  # for multi-head output
+        charge_output_index: Optional[int] = None,  # for multi-head output
     ):
         """
         Args:
@@ -57,6 +61,9 @@ class Atomwise(nn.Module):
         self.descriptor_output_key = descriptor_output_key
         if self.per_atom_output_key is not None:
             self.model_outputs.append(self.per_atom_output_key)
+        self.per_atom_output_key_2 = per_atom_output_key_2
+        if per_atom_output_key_2 is not None:
+            self.model_outputs.append(per_atom_output_key_2)
         if self.descriptor_output_key is not None: 
             self.model_outputs.append(self.descriptor_output_key)
 
@@ -80,6 +87,12 @@ class Atomwise(nn.Module):
         self.post_process = post_process
         self.bias = bias
         self.feature_key = feature_key
+        if self.n_out == 2:
+            self.energy_output_index = energy_output_index
+            self.charge_output_index = charge_output_index
+        else:
+            self.energy_output_index = None
+            self.charge_output_index = None
 
         if n_in is not None:
             self.outnet = build_mlp(
@@ -159,7 +172,14 @@ class Atomwise(nn.Module):
 
         # accumulate the per-atom output if necessary
         if self.per_atom_output_key is not None:
-            data[self.per_atom_output_key] = y
+            if self.energy_output_index is not None:
+                a = y[:, self.energy_output_index]
+                data[self.per_atom_output_key] = a
+
+        if self.per_atom_output_key_2 is not None:
+            if self.charge_output_index is not None:
+                a = y[:, self.charge_output_index]
+                data[self.per_atom_output_key_2] = a
 
         if hasattr(self, "descriptor_output_key") and self.descriptor_output_key is not None:
             data[self.descriptor_output_key] = features
@@ -177,5 +197,83 @@ class Atomwise(nn.Module):
 
         if hasattr(self, "post_process") and self.post_process is not None:
             y = self.post_process(y)
-        data[self.output_key] = y[:, output_index] if output_index is not None else y
+        data[self.output_key] = y[:, self.energy_output_index] if self.energy_output_index is not None else y
+
+        # for name, params in self.named_parameters():
+        #     print (f"Parameter: {name}, requires_grad: {params.requires_grad}, shape: {params.shape}")
+        #     if params.dim() >= 2:
+        #         print(params[0,0])
+        #     else:
+        #         print (params[0])
+
+        return data
+
+class Atomwise_linear(nn.Module):
+    """
+    testing purpose
+    """
+
+    def __init__(
+            self,
+            n_in: Optional[int] = None,
+            n_out: int = 1,
+            bias: bool = True,
+            activation: Callable = F.silu,
+            feature_key: Union[str, Sequence[int]] = 'node_feats',
+            per_atom_output_key: Optional[str] = None,
+            use_batchnorm: bool = False,
+            post_process: Optional[Callable] = None,
+            output_index: int = None, # only used for multi-head output
+        ):
+            super().__init__()
+            self.model_outputs = []
+            self.per_atom_output_key = per_atom_output_key
+            if self.per_atom_output_key is not None:
+                self.model_outputs.append(self.per_atom_output_key)
+
+            self.n_out = n_out
+
+            self.n_in = n_in
+            self.n_out = n_out
+            self.activation = activation
+            self.use_batchnorm = use_batchnorm
+            self.post_process = post_process
+            self.bias = bias
+            self.feature_key = feature_key
+            self.outnet = None
+            self.output_index = output_index
+
+    def forward(self, 
+                data: Dict[str, torch.Tensor], 
+                training: bool = None,
+                output_index: int = None, # only used for multi-head output
+               ) -> Dict[str, torch.Tensor]:
+        
+        if not hasattr(self, "feature_key") or self.feature_key is None: 
+            self.feature_key = "node_feats"
+
+        if isinstance(self.feature_key, str):
+            if self.feature_key not in data:
+                raise ValueError(f"Feature key {self.feature_key} not found in data dictionary.")
+            features = data[self.feature_key]
+            features = features.reshape(features.shape[0], -1)
+        elif isinstance(self.feature_key, list):
+            features = torch.cat([data[key].reshape(data[key].shape[0], -1) for key in self.feature_key], dim=-1)
+
+        if self.n_in is None:
+            self.n_in = features.shape[1]
+        else:
+            assert self.n_in == features.shape[1]
+
+        self.outnet = nn.Linear(self.n_in, self.n_out, self.bias)
+
+        # predict atomwise contributions
+        y = self.outnet(features)
+
+        # accumulate the per-atom output if necessary
+        if self.per_atom_output_key is not None:
+            if hasattr(self, "post_process") and self.post_process is not None:
+                y = self.post_process(y)
+            data[self.per_atom_output_key] = y
+        
         return data
